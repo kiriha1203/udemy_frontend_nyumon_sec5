b546e16f01e3195faff9e95faa9d3bcf70f7e543
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-11-13 00:51:26 +0900

    totyu

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    memos: [
+      { id: 1, body: 'サンプルのメモです'}
+    ]
+  },
+  mutations: {
+    save(state, memo) {
+      var max = state.memos[state.memos.length - 1]
+      state.memos.push(memo);
+    }
+  }
+})
