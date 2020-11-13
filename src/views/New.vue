f4ef547b415bd3f66d7b8baff0bc304e6243b7a8
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-11-13 17:36:20 +0900

    end

--- src/views/New.vue
+++ src/views/New.vue
@@ -1,28 +1,19 @@
 <template>
-  <div class="editor">
-      <h1>New Memo</h1>
+  <div class="new">
+      <Header>New Memo</Header>
       <textarea name="memo" v-model="memoBody"></textarea>
       <button @click="save">保存</button>
   </div>
 </template>
 
-<style scoped>
-textarea {
-  width: 100%;
-  height: 10em;
-}
-button {
-  border: 1px solid #333;
-  background-color: #333;
-  color: #fff;
-  padding: 10px 20px;
-  margin-top: 10px;
-}
-</style>
-
 <script>
+import Header from "@/components/Header.vue"
+
 export default {
   name: "new",
+  components: {
+    Header
+  },
   data: function() {
     return {
       memoBody: ""
@@ -31,7 +22,6 @@ export default {
   methods: {
     save: function() {
       this.$store.commit("save", {
-        id: 2,
         body: this.memoBody
       });
       this.$router.push("/");
